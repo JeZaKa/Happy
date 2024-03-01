c9b6848ec0434427467f43d2daca2c6f60a79b53
JeZaKa <zaicka.zhen@ukr.net>
2024-03-01 18:16:15 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav__wrapper');
+const overlay = document.querySelector('.overlay');
+const header = document.querySelector('.header')
+
+const burgerMenu = () => {
+  burger.classList.toggle('active')
+  nav.classList.toggle('active')
+  overlay.classList.toggle('active')
+  document.body.classList.toggle('disable-scroll')
+  if (nav.classList.contains('active')) {
+    header.classList.add('active')
+  } else {
+    header.classList.remove('active')
+  }
+}
+
+const closeBurgerMenu = () => {
+  burger.classList.remove('active')
+  nav.classList.remove('active')
+  overlay.classList.remove('active')
+  document.body.classList.remove('disable-scroll')
+  header.classList.remove('active')
+}
+
+burger.addEventListener('click', burgerMenu);
+nav.addEventListener('click', closeBurgerMenu);
+overlay.addEventListener('click', closeBurgerMenu);
+
